93a7ceba93957fdf1c88da5823267e57ad1f2346
Mrig26 <mrig.hardwell@gmail.com>
2020-10-09 21:18:13 +0530

    Finishing touch

--- changemac.py
+++ changemac.py
@@ -9,7 +9,7 @@ mac_id = input("New MAC id > ")
 
 print ("[*] Changing MAC addr for " + interface + " interface to " + mac_id)
 
-#Executing sequential commands for changing the MAC address
+#Executing sequential commands for changing the MAC address.
 
 subprocess.call(["sudo","ifconfig",interface,"down"])
 subprocess.call(["sudo","ifconfig",interface,"hw","ether",mac_id])
