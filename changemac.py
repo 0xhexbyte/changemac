c0935fe904b524b29a6d7d8627b4d5c62d4d3bb4
Mrig26 <mrig.hardwell@gmail.com>
2020-10-09 21:14:14 +0530

    Adding a few lines and supporting comments

--- changemac.py
+++ changemac.py
@@ -2,9 +2,16 @@
 
 import subprocess
 
+#Defining the variables and taking input from the user.
+
 interface = input("Interface >")
 mac_id = input("New MAC id > ")
+
+print ("[*] Changing MAC addr for " + interface + " interface to " + mac_id)
+
+#Executing sequential commands for changing the MAC address
 
 subprocess.call(["sudo","ifconfig",interface,"down"])
 subprocess.call(["sudo","ifconfig",interface,"hw","ether",mac_id])
 subprocess.call(["sudo","ifconfig",interface,"up"])
+	
