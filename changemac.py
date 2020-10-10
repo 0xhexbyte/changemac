4d7727274f2036642d5a263b7c89a7e1e96ce469
Mrig26 <mrig.hardwell@gmail.com>
2020-10-10 09:13:35 +0530

    UPgrading

--- changemac.py
+++ changemac.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python
 
 import subprocess
+import optparse
+
+def machanger(interface, mac_id):
+	print ("[*] Changing MAC addr for " + interface + " interface to " + mac_id)
+
+	#Executing sequential commands for changing the MAC address.
+
+	subprocess.call(["sudo","ifconfig",interface,"down"])
+	subprocess.call(["sudo","ifconfig",interface,"hw","ether",mac_id])
+	subprocess.call(["sudo","ifconfig",interface,"up"])
+	
+
+
+#Defining our parser variable, which will parse user supplied command-line arguments
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Mention the interface name whose MAC address has to be changed." )
+parser.add_option("-m", "--mac", dest="mac_id", help="Enter the desired mac address." )
+
+(options, arguments) = parser.parse_args()
 
 #Defining the variables and taking input from the user.
 
-interface = input("Interface > ")
-mac_id = input("New MAC id > ")
+interface = options.interface
+mac_id = options.mac_id
 
-print ("[*] Changing MAC addr for " + interface + " interface to " + mac_id)
+#Calling the function
 
-#Executing sequential commands for changing the MAC address.
-
-subprocess.call(["sudo","ifconfig",interface,"down"])
-subprocess.call(["sudo","ifconfig",interface,"hw","ether",mac_id])
-subprocess.call(["sudo","ifconfig",interface,"up"])
-	
+machanger(interface, mac_id)
